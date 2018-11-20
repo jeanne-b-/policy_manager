097fac95a0b1567f122e041d8ae9bad815d69ca2
jeanne <jeanne.bailhache@gmail.com>
2018-11-20 15:10:41 +0100

    portability_request : add option to notify other services + modal to confirm

--- lib/migrations/create_policy_manager_portability_requests.rb
+++ lib/migrations/create_policy_manager_portability_requests.rb
@@ -4,6 +4,8 @@ class CreatePolicyManagerPortabilityRequests < ActiveRecord::Migration[5.0]
       t.references :owner, polymorphic: true, index: {name: 'index_p_m_portability_requests_on_owner_type_and_owner_id'}
       t.string :attachement
       t.string :state
+      t.string :requested_by
+      t.boolean :notify_other_services, nil: false, default: false
       t.datetime :expire_at
       t.timestamps
     end
