8c97674eea7673368919f92e79387bccb38d0229
jeanne <jeanne.bailhache@gmail.com>
2018-11-20 16:24:11 +0100

    set current_user

--- app/controllers/policy_manager/application_controller.rb
+++ app/controllers/policy_manager/application_controller.rb
@@ -1,12 +1,18 @@
 module PolicyManager
   class ApplicationController < ActionController::Base
     layout 'policy_manager'
+    before_filter :set_current_user
     before_filter :merge_abilities
+
 
     private
 
     def merge_abilities
-      current_ability.merge(PolicyManager::Ability.new(current_user))
+      current_ability.merge(PolicyManager::Ability.new(@current_user))
+    end
+    
+    def set_current_user
+      @current_user = current_user || super
     end
   end
 end
