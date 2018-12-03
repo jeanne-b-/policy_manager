08fa0865cffe0ebfa1a68ebbc538c6fe2ab9c962
Gargamel <gargamel@MacBook-Pro-de-Quentin-3.local>
2018-12-03 15:46:50 +0100

    change anonymization default option

--- lib/policy_manager/config.rb
+++ lib/policy_manager/config.rb
@@ -7,11 +7,11 @@ module PolicyManager
       yield self
       self
       @@is_admin_method ||= -> (user) { user.is_admin? }
-      @@can_ask_anonymization ||= -> (user) { true }
+      @@can_ask_anonymization ||= -> (user) { false }
       @@anonymize_method ||= :anonymize!
       @@user_resource ||= User
       @@from_email ||= 'privacy@42.fr'
-      @@user_language ||= -> (user) {:en}
+      @@user_language ||= -> (user) { :en }
       @@finder ||= :id
       @@portability_path ||= "/policies/portability_requests/api_create"
       @@anonymize_path ||= "/policies/anonymize_requests/api_create"
