5a30910c5ab195bfa7ad1be63dba5b2e0bbc6363
Gargamel <gargamel@MacBook-Pro-de-Quentin-3.local>
2018-12-05 14:31:46 +0100

    change httpparty post params from query to body for async_call_service

--- app/models/policy_manager/anonymize_request.rb
+++ app/models/policy_manager/anonymize_request.rb
@@ -56,7 +56,7 @@ module PolicyManager
     def async_call_service(service_name)
       service = Config.other_services[service_name.to_sym]
       if service.respond_to?('[]', :host) # services must have a host in configuration file
-        response = HTTParty.post(service[:host] + Config.anonymize_path, query: encrypted_params_for_service(service_name)).response
+        response = HTTParty.post(service[:host] + Config.anonymize_path, body: encrypted_params_for_service(service_name)).response
       else
         return false
       end
