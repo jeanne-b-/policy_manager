a4ab6e841ffba09e5cf10858b4e1b55563824920
Gargamel <gargamel@MacBook-Pro-de-Quentin-3.local>
2018-12-18 17:29:03 +0100

    fix perform method

--- app/workers/policy_manager_worker.rb
+++ app/workers/policy_manager_worker.rb
@@ -2,7 +2,7 @@ class PolicyManagerWorker
   include Sidekiq::Worker if defined?(Sidekiq)
 
   def perform gid, key, action, options = {}
-    object = ::GlobalID::Locator.locate id
+    object = ::GlobalID::Locator.locate gid
     object.send(action, *options)
   end
 end
