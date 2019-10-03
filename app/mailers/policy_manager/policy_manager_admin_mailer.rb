3fc20a02b136cb37b85c300b4cbdcb7d04c8da95
Quentin <quentin@42.fr>
2019-10-03 16:21:03 +0200

    fix mailer for mailjet

--- app/mailers/policy_manager/policy_manager_admin_mailer.rb
+++ app/mailers/policy_manager/policy_manager_admin_mailer.rb
@@ -4,12 +4,12 @@ module PolicyManager
     layout 'policy_manager_admin_mailer'
 
     def send_mail opts
+      delivery_method_options ||= {}
       if Rails.env.production? and Config.mailjet and Config.mailjet_api_key
-        delivery_method_options ||= {}
-        delivery_method_options.merge({ version: 'v3.1', api_key: Config.mailjet_api_key, secret_key: Config.mailjet_api_secret })
+        delivery_method_options.merge!({ version: 'v3.1', api_key: Config.mailjet_api_key, secret_key: Config.mailjet_api_secret })
       end
 
-      mail(opts)
+      mail(opts.merge({ delivery_method_options: delivery_method_options }))
     end
 
     def portability_requested(portability_request_id)
@@ -17,8 +17,6 @@ module PolicyManager
       @user = @portability_request.owner
       @link = policy_manager.admin_portability_requests_url
       opts = { from: Config.from_email, to: Config.dpo_email, subject: I18n.t("policy_manager.policy_manager_admin_mailer.portability_requested.subject") }
-      opts.merge!({
-      })
       
       set_mail_lang
 
@@ -30,8 +28,6 @@ module PolicyManager
       @user = @anonymize_request.owner
       @link = policy_manager.admin_anonymize_requests_url
       opts = { from: Config.from_email, to: Config.dpo_email, subject: I18n.t("policy_manager.policy_manager_admin_mailer.anonymize_requested.subject") }
-      opts.merge!({
-      })
       
       set_mail_lang
 
--- app/mailers/policy_manager/policy_manager_mailer.rb
+++ app/mailers/policy_manager/policy_manager_mailer.rb
@@ -3,12 +3,12 @@ module PolicyManager
     include Rails.application.routes.url_helpers
 
     def send_mail opts
+      delivery_method_options ||= {}
       if Rails.env.production? and Config.mailjet and Config.mailjet_api_key
-        delivery_method_options ||= {}
-        delivery_method_options.merge({ version: 'v3.1', api_key: Config.mailjet_api_key, secret_key: Config.mailjet_api_secret })
+        delivery_method_options.merge!({ version: 'v3.1', api_key: Config.mailjet_api_key, secret_key: Config.mailjet_api_secret })
       end
 
-      mail(opts)
+      mail(opts.merge({ delivery_method_options: delivery_method_options }))
     end
 
     def portability_completed(portability_request_id)
@@ -16,8 +16,6 @@ module PolicyManager
       @user = @portability_request.owner
       @link = root_url + @portability_request.attachement_url
       opts = { from: Config.from_email, to: @user.email, subject: I18n.t("policy_manager.policy_manager_mailer.portability_completed.subject") }
-      opts.merge!({
-      })
       
       set_mail_lang
 
@@ -29,8 +27,6 @@ module PolicyManager
       @user = @portability_request.owner
       @link = policy_manager.portability_requests_url
       opts = { from: Config.from_email, to: @user.email, subject: I18n.t("policy_manager.policy_manager_mailer.portability_requested.subject") }
-      opts.merge!({
-      })
       
       set_mail_lang
 
@@ -42,8 +38,6 @@ module PolicyManager
       @user = @portability_request.owner
       @link = policy_manager.portability_requests_url
       opts = { from: Config.from_email, to: @user.email, subject: I18n.t("policy_manager.policy_manager_mailer.portability_denied.subject") }
-      opts.merge!({
-      })
       
       set_mail_lang
 
@@ -55,8 +49,6 @@ module PolicyManager
       @user = @anonymize_request.owner
       @link = policy_manager.anonymize_requests_url
       opts = { from: Config.from_email, to: @user.email, subject: I18n.t("policy_manager.policy_manager_mailer.anonymize_requested.subject") }
-      opts.merge!({
-      })
       
       set_mail_lang
 
@@ -68,8 +60,6 @@ module PolicyManager
       @user = @anonymize_request.owner
       @link = policy_manager.anonymize_requests_url
       opts = { from: Config.from_email, to: @user.email, subject: I18n.t("policy_manager.policy_manager_mailer.anonymize_denied.subject") }
-      opts.merge!({
-      })
       
       set_mail_lang
 
